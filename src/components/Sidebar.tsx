ff5cf59b93e26b6950815b9540ed55bd80ed9a16
focta <focke.wulf7234@gmail.com>
2023-05-22 18:49:19 +0900

    muiを導入してSidebarのデザインを整える

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,16 +1,24 @@
 import React from "react";
+import logo from './logo.svg';
 import "./Sidebar.scss";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      
       <div className="sidebarLeft">
-
-        
+        <div className="serverIcon">
+          <img src="./logo192.png" alt="" />
+        </div>
+        <div className="serverIcon">
+          <img src="./logo192.png" alt="" />
+        </div>
       </div>
       <div className="sidebarRight">
-
+        <div className="sidebarTop">
+          <h3>discord</h3>
+          <ExpandMoreIcon />
+        </div>
       </div>
     </div>
   );
