277151c9ec3f36d822c50414e0ac5fc743b254ae
focta <focke.wulf7234@gmail.com>
2023-05-23 07:15:45 +0900

    2行目のチャンネルレイアウトを実装した

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import logo from './logo.svg';
+import logo from "./logo.svg";
 import "./Sidebar.scss";
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import AddIcon from '@mui/icons-material/Add';
 
 function Sidebar() {
   return (
@@ -18,6 +19,15 @@ function Sidebar() {
         <div className="sidebarTop">
           <h3>discord</h3>
           <ExpandMoreIcon />
+        </div>
+        <div className="sidebarChannels">
+          <div className="sidebarChannelsHeader">
+            <div className="sidebarHeader">
+              <ExpandMoreIcon />
+              <h4>プログラミングチャンネル</h4>
+            </div>
+            <AddIcon className="sidebarAddIcon"/>
+          </div>
         </div>
       </div>
     </div>
